0388ff20df0a3040b1994ce498412fb1902ae41d
tsmada <adam@serouslabs.com>
2020-07-01 14:08:16 -0400

    feat: validate incoming JSON message with JSON schema

--- models/crash/index.js
+++ models/crash/index.js
@@ -1,9 +1,17 @@
 import fs from 'fs';
+import Ajv from 'ajv';
 
-let rawdata = fs.readFileSync('./models/crash/crash.json');
-const Crash = JSON.parse(rawdata)
-Crash.validate = function(model) {
-  console.log('Model: ', model);
-  return model
-}
+const ajv = new Ajv();
+const rawdata = fs.readFileSync('./models/crash/crash.json');
+const Crash = JSON.parse(rawdata);
+Crash.validate = function (model) {
+  let message;
+  try {
+    message = ajv.addSchema(Crash, 'crash').validate('crash', model);
+  } catch (err) {
+    console.log('Model invalid...', message);
+    return model;
+  }
+  return model;
+};
 export default Crash;
